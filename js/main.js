6f91f83502eb0e42fa76a7d37421e6bc88d59aee
David Sabine <DavidSabine@users.noreply.github.com>
2017-08-08 23:02:09 -0400

    Code cleanup: getting rid of main.css
    Replace main.css with typeface.css and decor.css

--- js/main.js
+++ js/main.js
@@ -17,18 +17,6 @@ $(document).ready(function () {
     setInterval(writeRandomCity,1750);
 
     /*=================================
-     ||          Turn off Google Map zooming
-     ==================================*/
-    $('.map_canvas').addClass('scrolloff'); // set the pointer events to none on doc ready
-    $('#map').on('click', function () {
-        $('.map_canvas').removeClass('scrolloff'); // set the pointer events true on click
-    });
-    // you want to disable pointer events when the mouse leave the canvas area;
-    $(".map_canvas").mouseleave(function () {
-        $('.map_canvas').addClass('scrolloff'); // set the pointer events to none when mouse leaves the map area
-    });
-
-    /*=================================
      ||          Nivo-lightbox
      ==================================*/
 
